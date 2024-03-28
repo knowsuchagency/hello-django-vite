ffb9f7a926a8beb4f7cd0021040aa1775e465fdb
Stephan Fitzpatrick <knowsuchagency@gmail.com>
2024-03-28 00:50:35 -0700

    Squash all commits into a single commit

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Django Vite ReactJS example
 
 This repo is meant to be a relatively minimal vite reactjs integration on django using django-vite and whitenoise
+
+[Link](https://django-vite-example.knowsuchagency.com)
--- hello_vite/core/views.py
+++ hello_vite/core/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import os
 
 from hello_vite.route_decorator import Route
 
@@ -8,4 +9,10 @@ route = Route()
 
 @route("/")
 def index(request):
-    return render(request, "index.html")
+    return render(
+        request,
+        "index.html",
+        context={
+            "cf_token": os.environ.get("CLOUDFLARE_TOKEN"),
+        },
+    )
